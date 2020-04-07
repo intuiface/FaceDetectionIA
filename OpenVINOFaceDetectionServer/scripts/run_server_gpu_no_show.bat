e85129629a92def6efa330b9b7c7aaa822f13d58
Nicolas Daures <nicolasdaures@hotmail.fr>
2020-04-07 10:58:34 +0200

    OV-53: update batch size for head pose estimation.

--- OpenVINOFaceDetectionServer/scripts/run_server_gpu_no_show.bat
+++ OpenVINOFaceDetectionServer/scripts/run_server_gpu_no_show.bat
@@ -1,1 +1,1 @@
-OpenVINOFaceDetectionServer.exe -i cam -m .\models\face-detection-adas-0001.xml -m_ag .\models\age-gender-recognition-retail-0013.xml -m_em .\models\emotions-recognition-retail-0003.xml -m_hp .\models\head-pose-estimation-adas-0001.xml -d GPU -n_ag "32" -n_em "32" -no_show
+OpenVINOFaceDetectionServer.exe -i cam -m .\models\face-detection-adas-0001.xml -m_ag .\models\age-gender-recognition-retail-0013.xml -m_em .\models\emotions-recognition-retail-0003.xml -m_hp .\models\head-pose-estimation-adas-0001.xml -d GPU -n_ag "32" -n_em "32" -n_hp "32" -no_show
